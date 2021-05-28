1e5b22d4b85fd9226b1ffab2b06ef9a62fc93039
Jerome Trombetta <jrmtbta@gmail.com>
2021-05-28 19:22:33 +0200

    add butchery criteria

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -16,6 +16,7 @@ class FavoritesController < ApplicationController
                                                   doctor: params[:doctor].present?,
                                                   handiwork: params[:handiwork].present?,
                                                   grocery: params[:grocery].present?,
+                                                  butchery: params[:butchery].present?,
                                                   max_distance_km: params[:location][:max_distance_km].to_i,
                                                   age_average: params[:max_age_average].present? ? params[:max_age_average] : nil,
                                                   max_population: params[:max_population].present? ? params[:max_population] : nil)
